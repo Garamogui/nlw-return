dbb57bb5a8dd68d55c4ce39cd983933baf6670af
Gabriel Arom <gagvms2003@protonmail.com>
2024-05-20 17:15:00 -0300

    end of first class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import { Widget } from "./components/Widget"
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <Widget />
   )
 }
 
